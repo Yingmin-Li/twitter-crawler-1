c7b20baad2081dee052868596c21cc1a8497a23d
Lilykos <lilykosk@gmail.com>
2012-12-16 19:47:08 +0200

    Update src/twittercrawler/NotAnnotatedCrawler.java

--- src/twittercrawler/NotAnnotatedCrawler.java
+++ src/twittercrawler/NotAnnotatedCrawler.java
@@ -19,10 +19,10 @@ import twitter4j.auth.AccessToken;
 
 public class NotAnnotatedCrawler {
     
-    final String CONSUMER_KEY = "5fPDqW5R9GXo9bWH3C5w";
-    final String CONSUMER_KEY_SECRET = "yXljitRmLP3L6TIhOOVw73x48CzGqXvxDuLWXiZEOs4";
-    final String ACCESS_TOKEN = "974982476-zxZ81ypNUxqo00m1tN26Py8phw153WlVrkm4wvcR";
-    final String ACCESS_TOKEN_SECRET = "WNXNzgPzuDjaqojPYnPBhf01BClNnxDB8nmn8l33U";
+    final String CONSUMER_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    final String CONSUMER_KEY_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    final String ACCESS_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    final String ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
     Status status;
     
     public void run() throws FileNotFoundException, IOException { 
