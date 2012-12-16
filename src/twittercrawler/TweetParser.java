5539b6b2323567b907136812752643a0162c4865
Lilykos <lilykosk@gmail.com>
2012-12-16 19:23:11 +0200

    crawler

---
+++ src/twittercrawler/TweetParser.java
@@ -0,0 +1,86 @@
+/* * * * * * * * * *
+ * Edw kanoume parsing to tweet kai pairnoume tis pio syxnes lekseis
+ * vazoume oles tis lekseis se ena hash map kai ektypwnoume sto stdout tis pio syxnes
+ * dystyxws den voh8aei idiaitera giati oi lekseis pou emfanizontai sterountai synais8hmatos
+ * omws to afhnoume afou ylopoih8hke etsi ki alliws.
+ */
+package twittercrawler;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class TweetParser {
+    
+    private HashMap frequencyMap;
+    private String tweet;
+    
+    public void run() throws FileNotFoundException, IOException {
+        FileInputStream fInstream = new FileInputStream("notAnnotatedOut.txt");
+        DataInputStream in = new DataInputStream(fInstream);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        
+        frequencyMap = new HashMap();
+        String strLine;
+        
+        // Kanoume ton elegxo prwta gia ta not annotated
+        while ((strLine = br.readLine()) != null) {
+            int index = strLine.indexOf("\t");
+            String str2 = strLine.substring(index + "\t".length());
+            index = str2.indexOf("\t");
+            
+            tweet = str2.substring(index + "\t".length());
+            getWords(tweet);
+        }
+        
+        fInstream = new FileInputStream("annotatedOut.txt");
+        in = new DataInputStream(fInstream);
+        br = new BufferedReader(new InputStreamReader(in));
+        
+        // Kai meta gia ta annotated
+        while ((strLine = br.readLine()) != null) {
+            int index = strLine.lastIndexOf("\t");
+            String str2 = strLine.substring(0, index);
+            int index2 = str2.lastIndexOf("\t");
+            
+            tweet = strLine.substring(index2, index - 1);
+            getWords(tweet);
+        }
+        printMap(frequencyMap);
+    }
+
+    
+    public static void printMap(Map<String, String> map){
+        int value;
+        
+        for (Map.Entry entry : map.entrySet()) {
+            value = (int) entry.getValue();
+            
+            // choose the frequency of words, as most are found only once - twice
+            // a value of 5-10 is good enough
+            if (value > 10)
+                System.out.println("Key : " + entry.getKey() + "\t\t\tValue : " + value);
+        }
+    }
+    
+    // Code found HERE:
+    // http://wiki.answers.com/Q/Java_program_to_find_frequency_of_occurence_of_each_word_in_text_file    
+    public void getWords(String strLine) {
+        strLine = strLine.toLowerCase();
+        
+        StringTokenizer parser = new StringTokenizer(strLine, " \t\n\r\f.,;:!?\""); 
+        while (parser.hasMoreTokens()) { 
+            String currentWord = parser.nextToken(); 
+            Integer frequency = (Integer) frequencyMap.get(currentWord); 
+            if (frequency == null) {
+                frequency = 0; }  
+            frequencyMap.put(currentWord, frequency + 1); 
+        }
+    }
+}
